7ac8feeb02e50e458e6404a6edebef569c26f8f2
AnnaNesterenko <nura_arh@ukr.net>
2022-10-27 14:45:20 +0300

    baseName

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="Dashboard">
       <App />
     </BrowserRouter>
   </React.StrictMode>
